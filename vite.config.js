43e49a26ffa036ce1de96f42b1dd6c2992612e8a
DiMaSiKXXL <dkir3@mail.ru>
2026-09-01 22:50:23 +0800

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  base: '/Ever-Habit-2/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
